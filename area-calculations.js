014f132e8ddef747040c450f3a6bd35b0a22debe
Brenda <brendamvreal@gmail.com>
2022-01-14 23:02:16 -0500

    Adding small projects, ageCalculator, area-calculations, hundreds calculator and perfect-shot

---
+++ area-calculations.js
@@ -0,0 +1,15 @@
+function calculateRectangleArea(length, width) {
+  if (length >= 0 && width >= 0) {
+    return length * width;
+  }
+}
+function calculateTriangleArea(base, height) {
+  if (base >= 0 && height >= 0) {
+    return base * height / 2;
+  }
+}
+function calculateCircleArea(radius) {
+  if (radius >= 0) {
+    return radius * radius * Math.PI;
+  }
+}
