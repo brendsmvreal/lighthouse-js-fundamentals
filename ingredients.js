e17fc8e492d8cd544653fa7c1964b410ca22ff11
Brenda <brendamvreal@gmail.com>
2022-01-15 01:31:50 -0500

    for loop, while lopp and reversed loop through arrays

---
+++ ingredients.js
@@ -0,0 +1,17 @@
+// while loop 
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+// for loop 
+for (let i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+};
+// for loop printing array backwards
+ for (let i = ingredients.length -1; i >= 0; i--) {
+    console.log(ingredients[i]);
+}
+
+
